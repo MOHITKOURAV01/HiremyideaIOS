a5414716b185cffc3a30b2912894661927e8083a
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-04-26 08:23:48 +0530

    Set up app entry point and main window group

---
+++ CycleInsights/CycleInsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleInsightsApp.swift
+//  CycleInsights
+//
+//  Created by MOHIT KOURAV on 25/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CycleInsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            InsightsView()
+        }
+    }
+}
